92af1a94cdbcc1bcda3a4be4077714b2318986b7
John Hartung <narpas@live.com>
2018-04-12 23:17:52 -0400

    jquery
    get jquery in there

--- js/index.js
+++ js/index.js
@@ -1,49 +1,83 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
-    }
-};
+currentDrawPile = 0;
+
+function cityCard (city, color, special){
+	card = $("<div></div>")[0];
+	card.city = city;
+	card.color = color;
+	card.special = special;
+	card.location = "drawPile0";
+
+	card.updateLocation = function(){
+		$(this).detach();
+		this.generateHTML();
+		$("#" + this.location).append(this);
+	}
+
+	card.generateHTML = function(){
+		this.innerHTML = "";
+		$(this).append(city)
+		.addClass(color + "Card")
+		.addClass("card");
+	}
+
+	card.reveal = function(){
+		this.location = "discardPile";
+		update();
+
+	}
+	
+	$(card).click(function(){
+		this.reveal();
+	})
+
+	return card;
+}
+
+initCityCards = [
+	{city: "London",              color: "blue",       special: 0},
+	{city: "London",              color: "blue",       special: 0},
+	{city: "London",              color: "blue",       special: 0},
+	{city: "New York",            color: "blue",       special: 0},
+	{city: "New York",            color: "blue",       special: "fortified"},
+	{city: "New York",            color: "blue",       special: 0},
+	{city: "Jacksonville",        color: "yellow",     special: 0},
+	{city: "Jacksonville",        color: "yellow",     special: 0},
+	{city: "Jacksonville",        color: "yellow",     special: 0},
+	{city: "Istambul",            color: "black",      special: 0},
+	{city: "Istambul",            color: "black",      special: 0},
+	{city: "Istambul",            color: "black",      special: 0},
+	{city: "Atlanta",             color: "blue",       special: 0},
+	{city: "Washington",          color: "blue",       special: 0},
+	{city: "Washington",          color: "blue",       special: 0},
+	{city: "Washington",          color: "blue",       special: 0},
+	{city: "Sao Paulo",           color: "yellow",     special: 0},
+	{city: "Sao Paulo",           color: "yellow",     special: 0},
+	{city: "Sao Paulo",           color: "yellow",     special: 0}
+	]
+
+	cityCards = [];
+	
+	$.each(initCityCards, function(){
+		newCard = cityCard(this.city, this.color, this.special);
+		cityCards.push(newCard);
+	})
+
+function update(){
+	$.each(cityCards, function(){
+		this.updateLocation();
+	});
+	
+}
+
+function epidemic(){
+	currentDrawPile++;
+	$("#cardPiles")[0].prepend($("<div id='drawPile" + currentDrawPile + "' class='cardPile'></div>")[0]);
+	$.each(cityCards, function(){
+		if (this.location == "discardPile"){
+			this.location = "drawPile" + currentDrawPile;
+		}
+	});
+	update();
+}
