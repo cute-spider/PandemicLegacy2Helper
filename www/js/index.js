776c9b798d44ee8668c72e51a63f700ae47c88b3
John Hartung <narpas@live.com>
2018-04-15 14:48:40 -0400

    SundayUpdate
    Come on wooork!

--- www/js/index.js
+++ www/js/index.js
@@ -1,82 +1,160 @@
-
-
 currentDrawPile = 0;
 
-function cityCard (city, color, special){
+function cityCard (city, color, special, index, connected, innoculated){
 	card = $("<div></div>")[0];
 	card.city = city;
 	card.color = color;
 	card.special = special;
-	card.location = "drawPile0";
+	card.index = index;
+	card.connected = connected;
+	card.innoculated = innoculated;
+	
+	if (!connected)           {card.stack = "inTheBox";}
+		else if (innoculated) {card.stack = "box6";}
+		else                  {card.stack = "drawPile0";}
+	
 
-	card.updateLocation = function(){
+	card.updatestack = function(){
 		$(this).detach();
-		this.generateHTML();
-		$("#" + this.location).append(this);
+		if (!this.innoculated) {$("#" + this.stack).append(this)};
 	}
 
 	card.generateHTML = function(){
 		this.innerHTML = "";
-		$(this).append(city)
+		$(this).append("<div class='cityName'>" + city + "</div>");
+		if (gameSettings.boxSixOpen) {$(this).append("<div class='innoculateButton'>" + "\u26A0" + "</div>");}
+		$(this).append("<div class='citySpecial'>" + special + "</div>")
 		.addClass(color + "Card")
 		.addClass("card");
 	}
 
 	card.reveal = function(){
-		this.location = "discardPile";
+		this.stack = "discardPile";
 		update();
-
 	}
 	
-	$(card).click(function(){
-		this.reveal();
+	card.innoculate = function(){
+		x = confirm("Really innoculate " + this.city + "?") 
+		if (x){
+			$(this).detach();
+			this.innoculated = true;
+			this.stack = box6;
+		}
+	}
+	
+	$(card).click(function(e){
+		if ($(e.target).hasClass("innoculateButton")){
+			this.innoculate();
+		}
+		else {
+			this.reveal();
+		}
 	})
+	
+	
+		
+	card.generateHTML();
 
 	return card;
 }
 
 initCityCards = [
-	{city: "London",              color: "blue",       special: 0},
-	{city: "London",              color: "blue",       special: 0},
-	{city: "London",              color: "blue",       special: 0},
-	{city: "New York",            color: "blue",       special: 0},
-	{city: "New York",            color: "blue",       special: "fortified"},
-	{city: "New York",            color: "blue",       special: 0},
-	{city: "Jacksonville",        color: "yellow",     special: 0},
-	{city: "Jacksonville",        color: "yellow",     special: 0},
-	{city: "Jacksonville",        color: "yellow",     special: 0},
-	{city: "Istambul",            color: "black",      special: 0},
-	{city: "Istambul",            color: "black",      special: 0},
-	{city: "Istambul",            color: "black",      special: 0},
-	{city: "Atlanta",             color: "blue",       special: 0},
-	{city: "Washington",          color: "blue",       special: 0},
-	{city: "Washington",          color: "blue",       special: 0},
-	{city: "Washington",          color: "blue",       special: 0},
-	{city: "Sao Paulo",           color: "yellow",     special: 0},
-	{city: "Sao Paulo",           color: "yellow",     special: 0},
-	{city: "Sao Paulo",           color: "yellow",     special: 0}
-	]
-
-	cityCards = [];
+{city:"Los Angeles",     color:"yellow",  innoculated: false, connected: false,  special: ""},
+{city:"Denver",          color:"blue"  ,  innoculated: false, connected: false,  special: ""},
+{city:"Denver",          color:"blue"  ,  innoculated: false, connected: false,  special: ""},
+{city:"Khartoum",        color:"yellow",  innoculated: false, connected: false,  special: ""},
+{city:"Dar es Salaam",   color:"yellow",  innoculated: false, connected: false,  special: ""},
+{city:"Johannesburg",    color:"blue"  ,  innoculated: false, connected: false,  special: ""},
+{city:"Antananarivo",    color:"black" ,  innoculated: false, connected: false,  special: ""},
+{city:"Dar es Salaam",   color:"yellow",  innoculated: false, connected: false,  special: ""},
+{city:"Johannesburg",    color:"blue"  ,  innoculated: false, connected: false,  special: ""},
+{city:"Antananarivo",    color:"black" ,  innoculated: false, connected: false,  special: ""},
+{city:"Santiago",        color:"yellow",  innoculated: true,  connected: true,   special: ""},
+{city:"Cairo",           color:"black" ,  innoculated: true,  connected: true,   special: ""},
+{city:"Cairo",           color:"black" ,  innoculated: true,  connected: true,   special: ""},
+{city:"Mexico City",     color:"yellow",  innoculated: false, connected: true,   special: ""},
+{city:"Buenos Aires",    color:"yellow",  innoculated: false, connected: true,   special: ""},
+{city:"Buenos Aires",    color:"yellow",  innoculated: false, connected: true,   special: ""},
+{city:"Lima",            color:"yellow",  innoculated: false, connected: true,   special: ""},
+{city:"Bogota",          color:"yellow",  innoculated: false, connected: true,   special: ""},
+{city:"Bogota",          color:"yellow",  innoculated: false, connected: true,   special: ""},
+{city:"Kinshasa",        color:"yellow",  innoculated: false, connected: true,   special: ""},
+{city:"Lagos",           color:"yellow",  innoculated: false, connected: true,   special: ""},
+{city:"Jacksonville",    color:"yellow",  innoculated: false, connected: true,   special: ""},
+{city:"Sao Paulo",       color:"yellow",  innoculated: false, connected: true,   special: ""},
+{city:"Istanbul",        color:"black" ,  innoculated: false, connected: true,   special: ""},
+{city:"Tripoli",         color:"black" ,  innoculated: false, connected: true,   special: ""},
+{city:"Lagos",           color:"yellow",  innoculated: false, connected: true,   special: ""},
+{city:"Jacksonville",    color:"yellow",  innoculated: false, connected: true,   special: ""},
+{city:"Sao Paulo",       color:"yellow",  innoculated: false, connected: true,   special: ""},
+{city:"Istanbul",        color:"black" ,  innoculated: false, connected: true,   special: ""},
+{city:"Tripoli",         color:"black" ,  innoculated: false, connected: true,   special: ""},
+{city:"Lagos",           color:"yellow",  innoculated: false, connected: true,   special: ""},
+{city:"Jacksonville",    color:"yellow",  innoculated: false, connected: true,   special: ""},
+{city:"Sao Paulo",       color:"yellow",  innoculated: false, connected: true,   special: ""},
+{city:"Istanbul",        color:"black" ,  innoculated: false, connected: true,   special: ""},
+{city:"Tripoli",         color:"black" ,  innoculated: false, connected: true,   special: ""},
+{city:"Cairo",           color:"black" ,  innoculated: false, connected: true,   special: ""},
+{city:"Chicago",         color:"blue"  ,  innoculated: false, connected: true,   special: ""},
+{city:"Chicago",         color:"blue"  ,  innoculated: false, connected: true,   special: ""},
+{city:"London",          color:"blue"  ,  innoculated: false, connected: true,   special: ""},
+{city:"London",          color:"blue"  ,  innoculated: false, connected: true,   special: ""},
+{city:"London",          color:"blue"  ,  innoculated: false, connected: true,   special: ""},
+{city:"San Francisco",   color:"blue"  ,  innoculated: false, connected: true,   special: ""},
+{city:"San Francisco",   color:"blue"  ,  innoculated: false, connected: true,   special: ""},
+{city:"Atlanta",         color:"blue"  ,  innoculated: false, connected: true,   special: ""},
+{city:"Washington",      color:"blue"  ,  innoculated: false, connected: true,   special: ""},
+{city:"New York",        color:"blue"  ,  innoculated: false, connected: true,   special: ""},
+{city:"Washington",      color:"blue"  ,  innoculated: false, connected: true,   special: ""},
+{city:"New York",        color:"blue"  ,  innoculated: false, connected: true,   special: ""},
+{city:"Washington",      color:"blue"  ,  innoculated: false, connected: true,   special: ""},
+{city:"New York",        color:"blue"  ,  innoculated: false, connected: true,   special: ""}
+	];
 	
-	$.each(initCityCards, function(){
-		newCard = cityCard(this.city, this.color, this.special);
-		cityCards.push(newCard);
-	})
+initCityCards.sort(function (a, b) {
+    if (a.color + a.city < b.color + b.city)
+		return -1
+	else 
+		return 1
+})
+	
+gameSettings = {
+	boxSixOpen: true
+}
+
+cityCards = [];
+
+cardIndex = 0;
+$.each(initCityCards, function(){
+	newCard = cityCard(this.city, this.color, this.special, this.cardIndex, this.connected, this.innoculated);
+	cityCards.push(newCard);
+	cardIndex++;
+})
 
 function update(){
 	$.each(cityCards, function(){
-		this.updateLocation();
+		// Avoid constantly reordering, especially the discard pile. 
+		if (this.parentElement == null){
+			this.updatestack();
+		}
+		else if (this.parentElement.id != this.stack){
+			this.updatestack();
+		}
+	});
+	$.each($(".cardPile:not(#discardPile, #drawPile0)"), function(){
+		if (!this.hasChildNodes()){
+			this.remove();
+		}
 	});
 	
 }
 
 function epidemic(){
 	currentDrawPile++;
-	$("#cardPiles")[0].prepend($("<div id='drawPile" + currentDrawPile + "' class='cardPile'></div>")[0]);
+	$("#drawPiles")[0].prepend($("<div id='drawPile" + currentDrawPile + "' class='cardPile'></div>")[0]);
 	$.each(cityCards, function(){
-		if (this.location == "discardPile"){
-			this.location = "drawPile" + currentDrawPile;
+		if (this.stack == "discardPile"){
+			this.stack = "drawPile" + currentDrawPile;
 		}
 	});
 	update();
